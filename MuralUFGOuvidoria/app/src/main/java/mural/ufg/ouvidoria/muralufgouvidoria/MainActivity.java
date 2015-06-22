eec0811df6aaa2aae063eda1909e3621543ab3f5
Wallace Luis do Carmo filho <ninja_krexi@hotmail.com>
2015-06-22 17:33:15 -0300

    Navigation Drawer Fragment

--- MuralUFGOuvidoria/app/src/main/java/mural/ufg/ouvidoria/muralufgouvidoria/MainActivity.java
+++ MuralUFGOuvidoria/app/src/main/java/mural/ufg/ouvidoria/muralufgouvidoria/MainActivity.java
@@ -1,6 +1,7 @@
 package mural.ufg.ouvidoria.muralufgouvidoria;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +20,12 @@ public class MainActivity extends ActionBarActivity {
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
 
     }
 
